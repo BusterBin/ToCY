cd16d174df21344772d96a3b43f54e7a5128a29d
Camellia <1175438638@qq.com>
2026-09-10 22:37:32 +0800

    Update musicPlayer.js

--- musicPlayer/js/musicPlayer.js
+++ musicPlayer/js/musicPlayer.js
@@ -25,7 +25,7 @@ $(function () {
     var musicImgsData = ['musicPlayer/img/NeverLand.jpg','musicPlayer/img/OneLastKiss.jpg'];    // 图片地址数组
     var musicNameData = ['NeverLand','- - -'];                   // 歌曲名数组
     var artistNameData = ['Youzee','宇多田光'];            // 创作歌手数组
-    var musicUrls = ['musicPlayer/mp3/Youzee-NeverLand.mp3','musicPlayer/mp3/宇多田光 - OneLastKiss.mp3'];// 歌曲mp3数组
+    var musicUrls = ['musicPlayer/mp3/Youzee - NeverLand.mp3','musicPlayer/mp3/宇多田光 - OneLastKiss.mp3'];// 歌曲mp3数组
     var currIndex = -1;              // 当前播放索引
     
     var buffInterval = null;          // 初始化定时器 判断是否需要缓冲
